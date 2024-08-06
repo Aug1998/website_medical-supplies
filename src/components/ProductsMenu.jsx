765e7f168c6a99954605fb9600d1e55ece4bee41
Aug1998 <augustopruvost@gmail.com>
2024-08-06 20:57:03 -0300

    menu layout

--- src/components/ProductsMenu.jsx
+++ src/components/ProductsMenu.jsx
@@ -6,29 +6,29 @@ import Icon from './Icon'
 export default function ProductsMenu() {
   return (
     <Container>
-      <LeftNav>
-        <LeftNavOption>
+      <ProductsNav>
+        <ProductTypeColumn>
+          <h5>Cotilos</h5>
+          <a>Producto</a>
+          <a>Producto</a>
+          <a>Producto</a>
+          <a>Producto</a>
+        </ProductTypeColumn>
+        <ProductTypeColumn>
+          <h5>Cotilos</h5>
+          <a>Producto</a>
+        </ProductTypeColumn>
+      </ProductsNav>
+      <BodypartNav>
+        <BodypartItem>
+          <Icon icon="arrowRightSmall"/>
           Cadera
+        </BodypartItem>
+        <BodypartItem>
           <Icon icon="arrowRightSmall"/>
-        </LeftNavOption>
-        <LeftNavOption>
           Rodilla
-          <Icon icon="arrowRightSmall"/>
-        </LeftNavOption>
-      </LeftNav>
-      <RightNav>
-        <ProductTypeColumn>
-          <h5>Cotilos</h5>
-          <a>Producto</a>
-          <a>Producto</a>
-          <a>Producto</a>
-          <a>Producto</a>
-        </ProductTypeColumn>
-        <ProductTypeColumn>
-          <h5>Cotilos</h5>
-          <a>Producto</a>
-        </ProductTypeColumn>
-      </RightNav>
+        </BodypartItem>
+      </BodypartNav>
     </Container>
   )
 }
@@ -39,35 +39,41 @@ const Container = styled.div`
   top: 90px;
   right: 0;
   display: none;
+  display: flex;
   &:hover{
     display: flex;
   }
 `
 
-const LeftNav = styled.div`
+const BodypartNav = styled.div`
   background-color: ${colors.primary};
 `
 
-const LeftNavOption = styled.div`
-  width: 182px;
-  padding: 24px 24px;
+const BodypartItem = styled.div`
+  min-width: 160px;
+  padding: 23px 28px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   font-size: 12px;
   font-weight: 500;
   letter-spacing: 2px;
+  position: relative;
+  gap: 22px;
   &:hover{
     background-color: ${colors.primaryLight};
   }
   svg {
     fill: ${colors.accent};
+    transform: rotate(180deg);
   }
 `
 
-const RightNav = styled.div`
+const ProductsNav = styled.div`
   width: 100%;
   display: flex;
+  padding: 23px 52px 28px;
+  gap: 56px;
 `
 
 const ProductTypeColumn = styled.div`
@@ -75,8 +81,8 @@ const ProductTypeColumn = styled.div`
   width: fit-content;
   display: flex;
   flex-direction: column;
+  align-items: flex-end;
   gap: 16px;
-  padding: 24px 68px 26px 28px;
   font-family: 'Open Sans';
   h5 {
     color: ${colors.accent};
@@ -90,7 +96,6 @@ const ProductTypeColumn = styled.div`
     line-height: 24px;
     text-transform: uppercase;
     letter-spacing: 2px;
-    padding-left: 14px;
     &:hover {
       text-decoration: underline;
     }
